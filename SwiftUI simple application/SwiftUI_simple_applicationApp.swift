6af686e69d740a0e09e1f2dd24c390de0214ce04
Gerald <geraldcerd@gmail.com>
2023-07-17 20:13:37 -0600

    Initial Commit

---
+++ SwiftUI simple application/SwiftUI_simple_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_simple_applicationApp.swift
+//  SwiftUI simple application
+//
+//  Created by Gerald on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_simple_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
